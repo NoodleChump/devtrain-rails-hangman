4e416c5f1e454f90356571d337b50f14df848087
Jordane Lew <jordane.lew@powershop.co.nz>
2016-01-08 16:31:09 +1300

    Identified new model functionality that needs to be implemented for the current views. Cleaned up player tests. Created a new method for finding all of a  player's games, and another for only those won

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,17 @@
 class Player < ActiveRecord::Base
-  has_many :hangman_state, :dependent => :destroy
+  has_many :hangman_states, :dependent => :destroy
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+
+  def games
+    hangman_states
+  end
+
+  def won_games
+    hangman_states.select { |game| game.won? }
+  end
+
+  def lost_games
+    hangman_states.select { |game| game.lost? }
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,27 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
+  let(:name) { "foobar" }
+  subject(:player) { Player.create(name: name) }
 
-  context "when creating a new player" do
-    let(:player) { Player.new }
+  context "when a player's name is too small" do
+    let(:name) { "f" }
+    it { is_expected.to_not be_valid }
+  end
 
-    it "shouldn't validate a blank player" do
-      expect(player.valid?).to eq false
+  context "when a player's name is too long" do
+    let(:name) { "foo" * 10 }
+    it { is_expected.to_not be_valid }
+  end
+
+  context "when a player's name is of suitable length" do
+    it { is_expected.to be_valid }
+  end
+
+  context "when a player's name is not unique" do
+    it "shouldn't be valid" do
+      player.save
+      expect(Player.create(name: name)).to_not be_valid
+    end
+  end
+
+  context "a player with a won and lost game" do
+    subject(:player) { Player.create!(name: "Player") }
+    let(:lives) { 1 }
+    let(:word1) { "a" }
+    let(:game1) { HangmanState.create!(word_to_guess: word1, number_of_lives: lives, player: player) }
+    let(:word2) { "b" }
+    let(:game2) { HangmanState.create!(word_to_guess: word2, number_of_lives: lives, player: player) }
+
+    before do
+      HangmanSpecHelper.make_guess(game1, word1)
+      HangmanSpecHelper.make_guess(game2, word1)
     end
 
-    it "shouldn't validate a player with a tiny name" do
-      player.name = "f"
-      expect(player.valid?).to eq false
+    it "should have the two games played" do
+      expect(player.games.count).to eq 2
+      expect([game1, game2].all? { |game| player.games.include?(game) }).to eq true
     end
 
-    it "shouldn't validate a player with too long of a name" do
-      player.name = "foo" * 10
-      expect(player.valid?).to eq false
+    it "should have the single game won" do
+      expect(player.won_games).to eq [game1]
     end
 
-    it "should validate a player with name of suitable length" do
-      player.name = "foobar"
-      expect(player.valid?).to eq true
+    it "should have the single game lost" do
+      expect(player.lost_games).to eq [game2]
     end
   end
 end
