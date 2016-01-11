6379cca9289c3b927a78a709b33e44e83295f3b8
Jordane Lew <jordane.lew@powershop.co.nz>
2016-01-11 14:33:19 +1300

    Created a new static pages controller with home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'home' => 'static_pages#home'
+  get 'about' => 'static_pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'hangman_states#index'
+  root 'static_pages#home'
 
   resources :hangman_states, :players
 
