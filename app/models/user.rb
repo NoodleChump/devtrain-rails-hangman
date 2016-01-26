8972d48973262f16a6c85891d79a8058d4bec7fa
Jordane Lew <jordane.lew@powershop.co.nz>
2016-01-26 13:50:04 +1300

    Finished adding session functionality. Added gravatar to user form with change link.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  attr_accessor :remember_token
+
+  #TODO Move into presenter?
   def won_games
     games.select(&:won?)
   end
@@ -29,5 +32,29 @@ class User < ActiveRecord::Base
     else
       won_games.count / lost_games.count.to_f
     end
+  end
+
+  #TODO Move into an auth decorator?
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125205135) do
+ActiveRecord::Schema.define(version: 20160126001812) do
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at",      null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160125205135) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
