d7c8d5111cecaaf55ec2ef7b9b536037c90fa0df
Jordane Lew <jordane.lew@powershop.co.nz>
2016-01-29 15:14:23 +1300

    Created session controller specs.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    p session_params
     user = User.find_by(email: session_params[:email])
     if user && user.authenticate(session_params[:password])
       log_in user
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  let(:valid_user) { User.create!(name: "user", email: "user@example.com", password: "password", password_confirmation: "password") }
+
+  let(:valid_login) { { email: "invalid.user@example.com", password: "password", remember_me: '0' } }
+  let(:invalid_login) { { email: "invalid.user@example.com", password: "invalid", remember_me: '0' } }
+
+  let(:valid_session) { {} }
 
   describe "GET #new" do
     it "returns http success" do
@@ -9,4 +15,37 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
+  describe "POST #create" do
+    context "when credentials are correct" do
+      it "logs the user in" do
+        post :create, {session: valid_login}, valid_session
+        expect(current_user?(valid_user))
+      end
+    end
+
+    context "when credentials are incorrect" do
+      it "re-renders the login page" do
+        post :create, {session: invalid_login}, valid_session
+        expect(response).to render_template('new')
+      end
+
+      it "has nobody logged in" do
+        post :create, {session: invalid_login}, valid_session
+        expect(logged_in?).to be false
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    context "when a user is logged in" do
+      before do
+        log_in valid_user
+      end
+
+      it "logs the user out" do
+        delete :destroy, {}, valid_session
+        expect(logged_in?).to be false
+      end
+    end
+  end
 end
