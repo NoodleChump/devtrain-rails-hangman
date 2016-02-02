82e35fc95bc45a5b7e516f5f73591db22208021d
Jordane Lew <jordane.lew@powershop.co.nz>
2016-02-03 10:26:43 +1300

    Created Capybara feature specs for login and logout flows.

---
+++ spec/features/login_flow_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+feature "Logging in" do
+  let(:name) { "a_user" }
+  let(:email) { "email@example.com" }
+  let(:password) { "abc123" }
+  let(:mismatched_password) { password.reverse + "!" }
+
+  let(:user) { User.create!(name: name, email: email, password: password, password_confirmation: password) }
+
+  scenario "Log in correctly as a user" do
+    visit_login_page
+    expect_to_see_login_form
+    enter_login_details
+    submit_page
+    expect_to_see_user_detail_page
+  end
+
+  scenario "Log in with a bad email/password combination" do
+    visit_login_page
+    expect_to_see_login_form
+    enter_bad_login_details
+    submit_page
+    expect_to_see_login_form
+    expect_to_see_invalid
+  end
+
+  def visit_login_page
+    visit login_url
+  end
+
+  def expect_to_see_login_form
+    expect(page).to have_content "Log In"
+    expect(page).to have_content "Email"
+    expect(page).to have_content "Password"
+    expect(page).to have_content "Remember"
+  end
+
+  def enter_login_details
+    fill_in 'session_email', with: user.email
+    fill_in 'session_password', with: password
+  end
+
+  def enter_bad_login_details
+    fill_in 'session_email', with: user.email
+    fill_in 'session_password', with: mismatched_password
+  end
+
+  def submit_page
+    find_button("Log In").click
+  end
+
+  def expect_to_see_user_detail_page
+    expect(page).to have_content name
+    expect(page).to have_content "User Statistics"
+  end
+
+  def expect_to_see_invalid
+    expect(page).to have_content "Invalid"
+  end
+end
---
+++ spec/features/logout_flow_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+feature "Logging out" do
+  let(:name) { "a_user" }
+  let(:email) { "email@example.com" }
+  let(:password) { "abc123" }
+
+  let(:user) { User.create!(name: name, email: email, password: password, password_confirmation: password) }
+
+  scenario "Log out when logged in" do
+    log_in_as_user
+    visit_home_page
+    expect_to_see_account_links
+    follow_logout_link
+    expect_to_see_home_page
+  end
+
+  private
+
+  def log_in_as_user
+    visit_login_page
+    enter_login_details
+    submit_page
+  end
+
+  def visit_home_page
+    visit root_url
+  end
+
+  def expect_to_see_account_links
+    expect(page).to have_content name
+    expect(page).to have_content "Settings"
+    expect(page).to have_content "Log Out"
+  end
+
+  def follow_logout_link
+    find_link("Log Out").click
+  end
+
+  def expect_to_see_home_page
+    expect(page).to have_content "Rails Hangman"
+    expect(page).to have_content "Welcome"
+    expect(page).to have_content "Log In"
+  end
+
+  def visit_login_page
+    visit login_url
+  end
+
+  def enter_login_details
+    fill_in 'session_email', with: user.email
+    fill_in 'session_password', with: password
+  end
+
+  def submit_page
+    find_button("Log In").click
+  end
+end
