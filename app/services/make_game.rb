2f576de32a190abf5996d889123a1219031cbc56
Jordane Lew <jordane.lew@powershop.co.nz>
2016-02-09 16:33:48 +1300

    Created specs for new MakeGame service. Changed 'Player' column on players index to 'Name'.

--- app/services/make_game.rb
+++ app/services/make_game.rb
@@ -1,16 +1,15 @@
 class MakeGame
-  #TODO Make specs
   def initialize(user: nil, ranked: true, word_to_guess: nil, initial_number_of_lives: Game::DEFAULT_NUMBER_OF_LIVES)
     @user = user
-    @ranked = ranked
+    @custom = !ranked || word_to_guess.present? || initial_number_of_lives != Game::DEFAULT_NUMBER_OF_LIVES
     @word_to_guess = word_to_guess.blank? ? GenerateRandomWord.new.call : word_to_guess
     @initial_number_of_lives = initial_number_of_lives
   end
 
   def call
     @game = @user.games.create(
-      custom:           !@ranked,
-      word_to_guess:    @word_to_guess.downcase,
+      custom:                   @custom,
+      word_to_guess:            @word_to_guess.downcase,
       initial_number_of_lives:  @initial_number_of_lives
     )
   end
---
+++ spec/services/make_game_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe MakeGuess, type: :service do
+  let(:user) { create(:user) }
+
+  context "when making a call to make a game with only a user" do
+    subject(:game) { MakeGame.new(user: user).call }
+
+    it "makes a game with the default number of lives" do
+      expect(game.initial_number_of_lives).to eq Game::DEFAULT_NUMBER_OF_LIVES
+    end
+
+    it "chooses a random word to guess" do
+      guesses = Hash.new(false)
+      20.times do
+        guess = MakeGame.new(user: user).call
+        expect(guess.word_to_guess).to be_present
+        expect(guesses[guess.word_to_guess]).to eq false
+        guesses[guess.word_to_guess] = true
+      end
+    end
+
+    it "has the given user as the player" do
+      expect(game.user).to eq user
+    end
+
+    it { is_expected.to be_ranked }
+  end
+
+  context "when making a call to make a game specifying named params" do
+    let(:word) { "banana" }
+    let(:lives) { 3 }
+    let(:ranked) { true }
+
+    subject(:game) { MakeGame.new(
+      user:                     user,
+      word_to_guess:            word,
+      initial_number_of_lives:  lives,
+      ranked:                   ranked
+    ).call }
+
+    it { is_expected.to_not be_ranked } #custom word and lives should override given ranked param
+
+    it "has the custom word to guess" do
+      expect(game.word_to_guess).to eq word
+    end
+
+    it "has the custom number of lives" do
+      expect(game.initial_number_of_lives).to eq lives
+    end
+
+    it "has the given user as the player" do
+      expect(game.user).to eq user
+    end
+  end
+end
