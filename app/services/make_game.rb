9a6acaa923b39ff578854a95e0f0277e4d6e8b76
Jordane Lew <jordane.lew@powershop.co.nz>
2016-02-10 09:48:32 +1300

    Broke out MakeGame logic for a clearer understanding and readability of the service.

--- app/services/make_game.rb
+++ app/services/make_game.rb
@@ -1,16 +1,30 @@
 class MakeGame
   def initialize(user: nil, ranked: true, word_to_guess: nil, initial_number_of_lives: Game::DEFAULT_NUMBER_OF_LIVES)
     @user = user
-    @custom = !ranked || word_to_guess.present? || initial_number_of_lives != Game::DEFAULT_NUMBER_OF_LIVES
-    @word_to_guess = word_to_guess.blank? ? GenerateRandomWord.new.call : word_to_guess
+    @word_to_guess = word_to_guess
     @initial_number_of_lives = initial_number_of_lives
+    @custom = !ranked || is_custom?
   end
 
   def call
     @game = @user.games.create(
       custom:                   @custom,
-      word_to_guess:            @word_to_guess.downcase,
+      word_to_guess:            word_to_guess.downcase,
       initial_number_of_lives:  @initial_number_of_lives
     )
+  end
+
+  private
+
+  def is_custom?
+    @word_to_guess.present? || @initial_number_of_lives != Game::DEFAULT_NUMBER_OF_LIVES
+  end
+
+  def word_to_guess
+    if @word_to_guess.blank?
+      GenerateRandomWord.new.call
+    else
+      @word_to_guess
+    end
   end
 end
